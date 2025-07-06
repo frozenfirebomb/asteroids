ca5457f5961196c3bfe6de70b7a12aff118e8214
frozenfirebomb <bartvrugt@hotmail.com>
2025-07-06 11:41:02 -0400

    CH2 L6 from Asteroids

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
+from constants import PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -14,3 +15,15 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def rotate(self, dt):
+        self.rotation = (PLAYER_TURN_SPEED * dt) + self.rotation
+        return self.rotation
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
