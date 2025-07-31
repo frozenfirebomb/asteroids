d089690805f0656631e32d8d9d944faf69e5f1ba
frozenfirebomb <bartvrugt@hotmail.com>
2025-07-31 07:43:00 -0400

    Asteroids CH 2 L8

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
@@ -20,10 +26,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
